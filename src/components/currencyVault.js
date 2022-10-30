e99bf21d87da3e398103b64bbeb6c04a08eec3f5
mypttwo <mypt.two@gmail.com>
2022-10-30 18:27:20 +0530

    css improvement

---
+++ src/components/currencyVault.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const CurrencyVault = () => {
+  const [display, setDisplay] = useState(true);
+  const depositJSX = () => {
+    return <div class="card-body">Deposit</div>;
+  };
+  const redeemJSX = () => {
+    return <div class="card-body">Redeem</div>;
+  };
+  return (
+    <div class="card">
+      <div class="card-body">
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a
+              class={`nav-link ${display ? "active" : ""}`}
+              href="#"
+              onClick={() => setDisplay(true)}
+            >
+              Deposit
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" onClick={() => setDisplay(false)}>
+              Redeem
+            </a>
+          </li>
+        </ul>
+      </div>
+      {display ? depositJSX() : redeemJSX()}
+    </div>
+  );
+};
+export default CurrencyVault;
--- src/components/currencyVaultPage.js
+++ src/components/currencyVaultPage.js
@@ -1,14 +1,14 @@
+import CurrencyVault from "./currencyVault";
+
 const CurrencyVaultPage = () => {
   return (
     <div className="container">
       <div className="row mt-5">
         <div className="col"></div>
-        <div className="col-7"></div>
+        <div className="col-7">
+          <CurrencyVault></CurrencyVault>
+        </div>
         <div className="col"></div>
-      </div>
-      <div className="row mt-5">
-        <div className="col-sm-6"></div>
-        <div className="col-sm-6"></div>
       </div>
     </div>
   );
