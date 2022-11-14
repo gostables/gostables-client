4b80f853866e9394aca5f03d95aa7d0d6f979bbd
CosmicAnon <wescosmic@gmail.com>
2022-11-14 19:49:38 +0530

    wallet connect ui tweak

--- src/components/walletConnect.js
+++ src/components/walletConnect.js
@@ -26,7 +26,7 @@ const WalletConnect = () => {
           <path d="M7.5 1v7h1V1h-1z"/>
           <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
         </svg>
-          <div className={`${className} text-warning`}>Connect Walllet</div>
+          <div className={`${className} text-warning`}>Connecting Walllet...</div>
         </button>
         </>);
     case 1:
