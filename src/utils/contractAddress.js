82785025e5b126f446ef87aaab9c1987735d8c2c
CosmicAnon <wescosmic@gmail.com>
2023-04-05 15:33:21 +0200

    Update contractAddress.js

--- src/utils/contractAddress.js
+++ src/utils/contractAddress.js
@@ -1,6 +1,6 @@
 const gStableManagerContractAddress = "TZDofabgTUK43589ow9zD3LseNPmVYqk6g";
 const SwapUSDDContractAddress = "TRH2YqLLNM6ekvqvBa13wB9NeCzMw7n2zU";
-const VaultUSDDContractAddress = "TKNqfLVHx9YWRmNiY7EdvyjFf1uw33QYn2";
+const VaultUSDDContractAddress = "TArnA3jJaZLgqstK953Ct3Dz4tSo93YZe3";
 const MarketContractAddress = "TQq9o4PahyoLociVzCnBMRRDdPZrNNkW1f";
 
 const SwapGStableContractAddress = "TB2ZNssD5WvGa2HSLHiYwe4xbsqW7ywcHN";
