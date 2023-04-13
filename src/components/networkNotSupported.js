63f2cda13e8d83bfdcee1676e50e21e13f9cfae9
CosmicAnon <wescosmic@gmail.com>
2023-04-13 22:48:42 +0200

    Update networkNotSupported.js

--- src/components/networkNotSupported.js
+++ src/components/networkNotSupported.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const NetworkNotSupported = () => {
   return (
     <div className="alert alert-primary mt-5 text-center text-black" role="alert">
@@ -5,7 +7,7 @@ const NetworkNotSupported = () => {
       <p>
         This version of goStables Protocol is currently deployed on the <strong>Tron Mainnet</strong> network.<br/>
         Please switch your TronLink to Mainnet network to continue.<br/>
-        If you want to use the Nile Testnet version please go to <a href="https://nile.gostables.org">https://nile.gotables.org</a>
+        If you want to use the Nile Testnet version please go to <a href="https://nile.gostables.org">https://nile.gostables.org</a>
       </p>
     </div>
   );
